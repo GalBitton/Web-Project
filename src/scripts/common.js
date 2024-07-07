f755fb19a6c437947861f1638a9bafbaa6383ad1
Eyal Maklada <33735637+EyalMK@users.noreply.github.com>
2024-07-07 18:55:51 +0300

    Moved main.js to common.js

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,11 +1,29 @@
+const createMenuItem = (href, text, image = false, classes = []) => {
+    const link = document.createElement('a');
+    link.href = href;
+    link.classList.add('block', 'px-4', 'py-2', 'flex', 'justify-center', ...classes);
+
+    if (image) {
+        const img = document.createElement('img');
+        img.src = text;
+        img.classList.add('themeIcon', 'h-[2rem]', 'w-[2rem]');
+        img.alt = 'Toggle Theme';
+        link.appendChild(img);
+    } else {
+        link.textContent = text
+    }
+    return link;
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.body;
     const currentPath = window.location.pathname;
+    const inDashboard = currentPath === '/dashboard';
 
     const renderFooter = () => {
         const footerHTML = `
             <footer class="bg-white dark:bg-gray-800 text-black dark:text-white p-4 text-center flex justify-center items-center gap-2">
-                <p class="text-base">© FitTrackerPro - 2024 All Rights Reserved </p> - 
+                <p class="text-base">© NeuroSync - 2024 All Rights Reserved </p> - 
                 <a href="/terms-of-service" class="hover:text-[#0059ff] ${currentPath === '/terms-of-service' ? 'text-[#0059ff]' : ''}"> Terms of Use </a> - 
                 <a href="/privacy-policy" class="hover:text-[#0059ff] ${currentPath === '/privacy-policy' ? 'text-[#0059ff]' : ''}"> Privacy Policy </a>
             </footer>`;
@@ -21,6 +39,15 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const renderMenu = () => {
+        const menuItems = inDashboard ? [
+
+        ] : [
+            { name: 'Best Products', href: '#' },
+            { name: 'Reviews', href: '#' },
+            { name: 'Analysis', href: '#' },
+            { name: 'About Us', href: '#' },
+            { name: 'Contact', href: '#' }
+        ];
         const headerMenu = document.getElementById('header-menu');
         headerMenu.className = 'bg-gray-200 dark:bg-gray-800 text-black dark:text-white p-4 flex justify-between items-center';
 
@@ -38,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const dashboardHeading = document.createElement('h1');
         dashboardHeading.classList.add('text-lg', 'hover:text-[#0059ff]');
-        if (currentPath === '/dashboard') {
+        if (inDashboard) {
             dashboardHeading.classList.add('text-[#0059ff]');
         }
         const dashboardLink = document.createElement('a');
@@ -46,49 +73,44 @@ document.addEventListener('DOMContentLoaded', () => {
         dashboardLink.textContent = 'Dashboard';
         dashboardHeading.appendChild(dashboardLink);
         logoContainer.appendChild(dashboardHeading);
-
-        const profileContainer = document.createElement('div');
-        profileContainer.classList.add('mx-16');
-
-        const profileImg = document.createElement('img');
-        profileImg.src = '/assets/profilePicture.webp';
-        profileImg.alt = 'Top Image';
-        profileImg.classList.add('h-16', 'cursor-pointer');
-        profileImg.id = 'top-menu-button';
-        profileContainer.appendChild(profileImg);
-
-        const menu = document.createElement('div');
-        menu.id = 'top-menu';
-        menu.classList.add('items-center', 'absolute', 'right-0', 'mt-2', 'mx-2', 'w-48', 'bg-gray-200', 'dark:bg-gray-700', 'text-black', 'dark:text-white', 'rounded-lg', 'shadow-lg', 'divide-y', 'hidden');
-
-        const createMenuItem = (href, text, image = false, classes = []) => {
-            const link = document.createElement('a');
-            link.href = href;
-            link.classList.add('block', 'px-4', 'py-2', 'flex', 'justify-center', ...classes);
-            
-            if (image) {
-                const img = document.createElement('img');
-                img.src = text;
-                img.classList.add('themeIcon', 'h-[2rem]', 'w-[2rem]');
-                img.alt = 'Toggle Theme';
-                link.appendChild(img);
-            } else {
-                link.textContent = text
-            }
-            return link;
-        };
-
-        menu.appendChild(createMenuItem('#', '', true, ['theme-toggle']));
-        menu.appendChild(createMenuItem('#', 'Settings'));
-        menu.appendChild(createMenuItem('#', 'Logout'));
-
-        profileContainer.appendChild(menu);
         headerMenu.appendChild(logoContainer);
-        headerMenu.appendChild(profileContainer);
+
+        menuItems.forEach((item) => {
+            const menuItem = createMenuItem(item.href, item.name, false, ['text-lg', 'hover:text-[#0059ff]', 'mx-4', 'cursor-pointer', 'text-black', 'dark:text-white']);
+            headerMenu.appendChild(menuItem);
+        });
+
+        if (inDashboard) {
+            const profileContainer = document.createElement('div');
+            profileContainer.classList.add('mx-16');
+
+            const profileImg = document.createElement('img');
+            profileImg.src = '/assets/profilePicture.webp';
+            profileImg.alt = 'Top Image';
+            profileImg.classList.add('h-16', 'cursor-pointer');
+            profileImg.id = 'top-menu-button';
+            profileContainer.appendChild(profileImg);
+
+            const menu = document.createElement('div');
+            menu.id = 'top-menu';
+            menu.classList.add('items-center', 'absolute', 'right-0', 'mt-2', 'mx-2', 'w-48', 'bg-gray-200', 'dark:bg-gray-700', 'text-black', 'dark:text-white', 'rounded-lg', 'shadow-lg', 'divide-y', 'hidden');
+
+            menu.appendChild(createMenuItem('#', '', true, ['theme-toggle']));
+            menu.appendChild(createMenuItem('#', 'Settings'));
+            menu.appendChild(createMenuItem('#', 'Logout'));
+
+            profileContainer.appendChild(menu);
+            headerMenu.appendChild(profileContainer);
+        }
     };
 
     const renderThemeToggle = () => {
-        const themeToggleButton = document.querySelector('#top-menu .theme-toggle');
+        const headerMenu = document.getElementById('header-menu');
+        const themeToggleButton = inDashboard ? document.querySelector('#top-menu .theme-toggle') :
+            createMenuItem('#', '', true, ['theme-toggle']);
+        if (!inDashboard) {
+            headerMenu.appendChild(themeToggleButton);
+        }
         const themeIcon = document.querySelector('.themeIcon');
         const darkModeIcon = 'assets/dark-mode.svg';
         const lightModeIcon = 'assets/light-mode.svg';
@@ -119,8 +141,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const init = () => {
         renderFooter();
         renderMenu();
-        renderProfileMenu();
         renderThemeToggle();
+        if (!inDashboard) return;
+        renderProfileMenu();
     };
 
     init();
