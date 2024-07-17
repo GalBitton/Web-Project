2895813c8f4a0c0c7737eef0d34f24334dc5310c
Eyal Maklada <33735637+EyalMK@users.noreply.github.com>
2024-07-17 23:14:50 +0300

    1. Setup logger (untested) 2. Placeholder for integration tests 3. WIP JenkinsFile

--- server/logger.js
+++ server/logger.js
@@ -1,1 +1,65 @@
 'use strict';
+
+import 'winston-mongodb';
+import mongoose from 'mongoose';
+import winston, { format } from 'winston';
+import 'winston-daily-rotate-file';
+
+const logFormat = format.printf(({ timestamp, level, message, request }) => {
+    // Clone request.body and remove or mask sensitive fields
+    let bodyString = '';
+    if (request?.body) {
+        const safeBody = { ...request.body };
+        if (safeBody?.password) {
+            safeBody.password = '[REDACTED]'; // Masking the password
+        }
+        bodyString = `body: ${JSON.stringify(safeBody, null, 2)}`;
+    }
+
+    const userIdPart = request?.user ? `[Provided User ID: ${request.user}]` : '';
+    const requestData = request ? `\n\t\tUrl: ${request.url},\n\t\tmethod: ${request.method},\n\t\tparameters: ${JSON.stringify(request.params, null, 12)},\n\t\t${bodyString}` : '';
+    const formattedRequest = requestData === '' ? '' : `Request - ${requestData}`;
+
+    return `[${timestamp}] ${level}: ${message} ${userIdPart}\n${formattedRequest}\n`;
+});
+
+class Logger {
+    constructor(config) {
+        this.logger = null;
+        this.config = config;
+        this.createLogger();
+    };
+
+    createLogger() {
+        const options = this.config.log2file ? {
+            filename: 'logs/%DATE%.txt',
+            datePattern: 'DD-MM-YYYY',
+            zippedArchive: true,
+            maxSize: '2m',
+            level: this.config.level,
+        } : {
+            db: mongoose.connection.useDb('logs'),
+            options: { useUnifiedTopology: true },
+            collection: 'logs',
+            capped: false,
+            leaveConnectionOpen: true
+        };
+
+        const transport = this.config.log2file ?
+            new winston.transports.DailyRotateFile(options) :
+            new winston.transports.MongoDB(options);
+
+        this.logger = winston.createLogger({
+            format: winston.format.combine(
+                winston.format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
+                winston.format.errors({ stack: true }),
+                logFormat
+            ),
+            transports: [
+                transport
+            ],
+        });
+    };
+}
+
+export default Logger;
