dcad3924b7ee82afd0ac3ebbd99a079f3b45282f
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-12 18:53:57 +0300

    Document

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,5 +1,13 @@
 import mongoose from 'mongoose';
 
+/**
+ * Connects to a MongoDB database and switches to a specified database.
+ * 
+ * @param {string} url - The connection URL for MongoDB.
+ * @param {Object} logger - The logger object used to log connection status and errors.
+ * @param {Function} logger.info - Method to log informational messages.
+ * @param {Function} logger.error - Method to log error messages.
+ */
 const connect = (url, logger) => {
     mongoose.connect(url)
     .then(() => {
