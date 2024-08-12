6757a776d751eac70d47cd1d28caf4994d080f0f
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-12 19:17:40 +0300

    Document

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,6 +1,15 @@
+/**
+ * Middleware exports for error handling and rate limiting.
+ * @module middlewares
+ */
+
 import errorHandler from "./errorHandler.middleware.js";
 import { limiter } from "./rateLimiters.middleware.js";
 
+/**
+ * Error handling middleware.
+ * @type {Function}
+ */
 export {
     errorHandler,
     limiter
