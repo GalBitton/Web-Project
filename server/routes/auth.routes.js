dfab253e578266a7674df91cb28fe737ebea94be
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-12 19:19:44 +0300

    Document

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import { registerLimiter, loginLimiter } from '../middlewares/rateLimiters.middleware.js';
 
+/**
+ * @class AuthRouter
+ * @description Sets up authentication routes with associated middleware and controllers.
+ */
 class AuthRouter {
+    /**
+     * @constructor
+     * @param {Object} authController - Instance of the authentication controller.
+     * @param {Object} authMiddleware - Instance of the authentication middleware.
+     */
     constructor(authController, authMiddleware) {
         this._router = express.Router();
         this.authController = authController;
@@ -9,10 +18,19 @@ class AuthRouter {
         this._registerRoutes();
     }
 
+    /**
+     * @method getRouter
+     * @returns {express.Router} - The configured Express router.
+     */
     getRouter() {
         return this._router;
     }
 
+    /**
+     * @method _registerRoutes
+     * @description Registers routes for authentication endpoints with rate limiting middleware.
+     * @private
+     */
     _registerRoutes() {
         this._router.post('/register', registerLimiter, this.authController.register);
         this._router.post('/authenticate', loginLimiter, this.authController.authenticate);
