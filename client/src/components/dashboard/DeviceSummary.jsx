1f3af81694993c6c9486f275ac307d3648125599
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-13 03:21:08 +0300

    doc

--- client/src/components/dashboard/DeviceSummary.jsx
+++ client/src/components/dashboard/DeviceSummary.jsx
@@ -1,3 +1,13 @@
+/**
+ * Displays the selected brand and type.
+ * 
+ * @function
+ * @name DeviceSummary
+ * @param {Object} props - The component props.
+ * @param {string} props.selectedBrand - The selected brand.
+ * @param {string} props.selectedType - The selected type.
+ * @returns {React.ReactElement} The rendered component.
+ */
 const DeviceSummary = ({ selectedBrand, selectedType }) => (
     <div className="text-center">
         <h2 className="text-xl font-bold">{`Selected Brand: ${selectedBrand}`}</h2>
