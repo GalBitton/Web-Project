d8803bb420ecd768285c00198c72aee4aab6c0b3
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-13 03:22:43 +0300

    doc

--- client/src/components/form/forminput.jsx
+++ client/src/components/form/forminput.jsx
@@ -1,3 +1,18 @@
+/**
+ * Renders a styled input field with label and placeholder.
+ * 
+ * @component
+ * @name FormInput
+ * @param {Object} props - The component props.
+ * @param {string} props.id - The unique identifier for the input field.
+ * @param {string} props.name - The name attribute for the input field.
+ * @param {string} [props.type='text'] - The type of the input field (e.g., 'text', 'password', etc.).
+ * @param {string} props.value - The current value of the input field.
+ * @param {Function} props.onChange - Function to handle changes to the input field.
+ * @param {string} [props.placeholder] - The placeholder text for the input field.
+ * @param {boolean} [props.required=true] - Whether the input field is required.
+ * @returns {React.ReactElement} The rendered component.
+ */
 const FormInput = ({ id, name, type = "text", value, onChange, placeholder, required = true }) => (
     <div>
         <label htmlFor={id} className="sr-only">
