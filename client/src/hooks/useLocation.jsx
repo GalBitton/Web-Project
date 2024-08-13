52a0bc0ee13bf0dc97982b9651259fbb3051f0a6
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-13 03:34:40 +0300

    doc

--- client/src/hooks/useLocation.jsx
+++ client/src/hooks/useLocation.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { useLocation as useRouterLocation } from 'react-router-dom';
 
+/**
+ * A custom hook that provides information about the current location and
+ * whether the current path is the dashboard.
+ * 
+ * Provides `location` from `react-router-dom` and a boolean `inDashboard`
+ * indicating if the current path is the dashboard route.
+ * 
+ * @returns {Object} An object containing the current location and a boolean indicating if it is the dashboard.
+ * @property {Object} location - The current location object from `react-router-dom`.
+ * @property {boolean} inDashboard - `true` if the current path is '/dashboard', otherwise `false`.
+ */
 const useLocation = () => {
     const location = useRouterLocation();
     const inDashboard = React.useMemo(() => location.pathname === '/dashboard', [location.pathname]);
