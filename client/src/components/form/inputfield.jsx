d8eff00fb0f9fbf767de131a3b1c20a5ede441af
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-13 03:22:22 +0300

    doc

--- client/src/components/form/inputfield.jsx
+++ client/src/components/form/inputfield.jsx
@@ -1,3 +1,17 @@
+/**
+ * Renders an input field or textarea with a label.
+ * 
+ * @component
+ * @name InputField
+ * @param {Object} props - The component props.
+ * @param {string} props.label - The label text for the input field.
+ * @param {string} props.id - The unique identifier for the input field.
+ * @param {string} [props.type='text'] - The type of the input field. Can be 'text' or 'textarea'.
+ * @param {string} props.value - The current value of the input field.
+ * @param {Function} props.onChange - Function to handle changes to the input field.
+ * @param {string} [props.placeholder] - The placeholder text for the input field.
+ * @returns {React.ReactElement} The rendered component.
+ */
 const InputField = ({ label, id, type = 'text', value, onChange, placeholder }) => {
     const classes = 'mt-2 p-3 w-full border rounded-lg bg-gray-100 dark:bg-gray-700 text-black dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-600';
     return (
