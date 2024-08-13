cca7fc183da80c6d67704bf5bf8e89052cfb5ea9
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-13 03:32:42 +0300

    doc

--- client/src/hooks/useAPIService.jsx
+++ client/src/hooks/useAPIService.jsx
@@ -1,6 +1,18 @@
 import { useEffect, useState } from 'react';
 import APIService from '../services/api/APIService.js';
 
+/**
+ * Custom hook for managing API service requests.
+ * 
+ * @param {Object} request - The request configuration for the API service.
+ * @param {string} request.action - The action to be performed by the API service.
+ * @param {Object} [request.params] - Optional parameters for the API request.
+ * 
+ * @returns {Object} The hook's state.
+ * @returns {any} data - The data retrieved from the API request.
+ * @returns {Error|null} error - Any error that occurred during the API request.
+ * @returns {boolean} loading - Whether the API request is in progress.
+ */
 const useAPIService = (request) => {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
