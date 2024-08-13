7bc6cd05a2113b80f050169053d6fcdcf7a253e6
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-13 03:32:25 +0300

    doc

--- client/src/hooks/AppProvider.jsx
+++ client/src/hooks/AppProvider.jsx
@@ -1,6 +1,14 @@
 import { useEffect } from "react";
 import serviceData from "../../package.json";
 
+/**
+ * Provides global styles and sets the document title based on the application's name.
+ * 
+ * @param {Object} props - Component props.
+ * @param {React.ReactNode} props.children - Child components to render within the provider.
+ * 
+ * @returns {JSX.Element} The `AppProvider` component with global styles and title settings.
+ */
 export const AppProvider = ({ children }) => {
     useEffect(() => {
         const title = document.createElement("title");
