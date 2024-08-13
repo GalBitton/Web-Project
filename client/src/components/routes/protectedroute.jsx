2c151201b0e444671566b7e768fc998c0b37fa8c
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-13 03:29:21 +0300

    doc

--- client/src/components/routes/protectedroute.jsx
+++ client/src/components/routes/protectedroute.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '@/contexts/AuthContext';
 
+/**
+ * A route component that renders its children if the user is logged in,
+ * otherwise redirects to a specified path.
+ *
+ * @component
+ * @example
+ * // Renders the children if the user is logged in, otherwise redirects to '/login'.
+ * return <ProtectedRoute redirectTo="/login" />;
+ *
+ * @param {Object} props - The component props.
+ * @param {string} [props.redirectTo='/login'] - The path to redirect to if the user is not logged in.
+ * 
+ * @returns {JSX.Element} The rendered component.
+ */
 const ProtectedRoute = ({ redirectTo = '/login' }) => {
     const { isLoggedIn } = useAuth();
 
--- client/src/components/routes/publicroute.jsx
+++ client/src/components/routes/publicroute.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '@/contexts/AuthContext';
 
+/**
+ * A route component that renders its children if the user is not logged in,
+ * otherwise redirects to a specified path.
+ *
+ * @component
+ * @example
+ * // Renders the children if the user is not logged in, otherwise redirects to '/dashboard'.
+ * return <PublicRoute redirectTo="/dashboard" />;
+ *
+ * @param {Object} props - The component props.
+ * @param {string} [props.redirectTo='/dashboard'] - The path to redirect to if the user is logged in.
+ * 
+ * @returns {JSX.Element} The rendered component.
+ */
 const PublicRoute = ({ redirectTo = '/dashboard' }) => {
     const { isLoggedIn } = useAuth();
 
