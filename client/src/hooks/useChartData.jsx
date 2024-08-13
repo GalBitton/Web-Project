605a1bc896892d523d121115aec25481ff8e6d45
Adir111 <118291963+Adir111@users.noreply.github.com>
2024-08-13 03:36:32 +0300

    doc

--- client/src/hooks/useChartData.jsx
+++ client/src/hooks/useChartData.jsx
@@ -1,5 +1,32 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * Custom hook to manage chart data for a device.
+ *
+ * @param {Object} currentDevice - The currently selected device for which to fetch data.
+ * @param {Object} avgData - The average data for various metrics.
+ * @param {Object} avgData.heartRateAverages - Average heart rate data.
+ * @param {Object} avgData.stepsAverages - Average steps data.
+ * @param {Object} avgData.caloriesAverages - Average calories data.
+ * @param {Object} avgData.sleepAverages - Average sleep data.
+ *
+ * @returns {Object} - Contains the chart data and a function to update it.
+ * @returns {Object} chartsData - Current chart data.
+ * @returns {Object} chartsData.heartRate - Heart rate data with `labels` and `values`.
+ * @returns {Object} chartsData.steps - Steps data with `labels` and `values`.
+ * @returns {Object} chartsData.calories - Calories data with `labels` and `values`.
+ * @returns {Object} chartsData.sleep - Sleep data with `labels`, `values`, and `valuesY1`.
+ * @returns {Object} chartsData.stress - Stress data with `labels` and `values`.
+ * @returns {Object} chartsData.oxygen - Oxygen data with `labels` and `values`.
+ * @returns {Object} chartsData.bloodPressure - Blood pressure data with `labels`, `systolic`, and `diastolic`.
+ * @returns {Object} chartsData.eeg - EEG data with `labels`, `alpha`, `beta`, `gamma`, `delta`, and `theta`.
+ * @returns {Object} averageChartsData - Average chart data.
+ * @returns {Object} averageChartsData.heartRate - Average heart rate data with `labels` and `values`.
+ * @returns {Object} averageChartsData.steps - Average steps data with `labels` and `values`.
+ * @returns {Object} averageChartsData.calories - Average calories data with `labels` and `values`.
+ * @returns {Object} averageChartsData.sleep - Average sleep data with `labels`, `values`, and `valuesY1`.
+ * @returns {Function} updateCharts - Function to update the chart data.
+ */
 const useChartData = (currentDevice, avgData) => {
     const [chartsData, setChartsData] = useState({
         heartRate: { labels: [], values: [] },
